9fbbe867ad6c86592af73ba998778eb2658b9ae9
Kevin Chaney <kevin_chaney@icloud.com>
2022-04-29 11:08:51 -0700

    commit tree a6e182f08deb778ac22fbb165893b5a453a36163

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(input = "*") {
+    const result = function (adjective = "special") {
+        return `You are ${input}${adjective}${input}!`;
+    };
+    return result;
+}
